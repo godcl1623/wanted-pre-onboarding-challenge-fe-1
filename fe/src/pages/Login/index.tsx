732a74fab48cd70729ec795c2dc22d75598e47e9
godcl1623 <delphi5281@gmail.com>
2022-08-07 18:01:02 +0900

    #1 design : 로그인 페이지 기본 요소 배치

--- fe/src/pages/Login/index.tsx
+++ fe/src/pages/Login/index.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
-  return <h1>Login</h1>;
+  return (
+    <main>
+      <form>
+        <label htmlFor="id_input">
+          ID:
+          <input type="text" name="id_input" />
+        </label>
+        <label htmlFor="password_input">
+          PW:
+          <input type="text" name="password_input" />
+        </label>
+        <input type="submit" name="login_button" value="로그인" />
+        <Link to="/signup">회원가입</Link>
+      </form>
+    </main>
+  );
 }

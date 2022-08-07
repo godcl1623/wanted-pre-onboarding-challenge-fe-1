72f9f7cd4ff4feea833ff8b965ca282d2bbf6f91
godcl1623 <delphi5281@gmail.com>
2022-08-07 18:26:33 +0900

    #1 design : 로그인 페이지 스타일 작성

--- fe/src/pages/Login/index.tsx
+++ fe/src/pages/Login/index.tsx
@@ -3,18 +3,31 @@ import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
-    <main>
-      <form>
-        <label htmlFor="id_input">
+    <main className="flex-center w-full h-full bg-slate-100">
+      <form className="flex-center flex-col w-[500px] h-[300px] border-solid rounded-lg shadow-xl bg-slate-50">
+        <label htmlFor="id_input" className="login-input-area">
           ID:
-          <input type="text" name="id_input" />
+          <input type="text" name="id_input" className="login-input" />
         </label>
-        <label htmlFor="password_input">
+        <label htmlFor="password_input" className="login-input-area">
           PW:
-          <input type="text" name="password_input" />
+          <input
+            type="password"
+            name="password_input"
+            className="login-input"
+          />
         </label>
-        <input type="submit" name="login_button" value="로그인" />
-        <Link to="/signup">회원가입</Link>
+        <div className="flex-center flex-col w-full py-5">
+          <input
+            type="submit"
+            name="login_button"
+            value="로그인"
+            className="login-area-buttons mb-3 bg-blue-300"
+          />
+          <Link to="/signup" className="login-area-buttons bg-red-300">
+            회원가입
+          </Link>
+        </div>
       </form>
     </main>
   );

82063e923510addcc8b22cabb5a0b3700d92a82a
godcl1623 <delphi5281@gmail.com>
2022-08-07 22:52:59 +0900

    #3 feat : 로그인 인증 기능 구현

--- fe/src/pages/Login/index.tsx
+++ fe/src/pages/Login/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import FormInput from 'components/FormInput';
+import loginController from './controllers/loginController';
 
 export default function Login() {
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    const formEventTarget = event.target as HTMLFormElement;
+    const emailInput = formEventTarget[0] as HTMLInputElement;
+    const passwordInput = formEventTarget[1] as HTMLInputElement;
+    try {
+      const loginResult = await loginController(
+        emailInput.value,
+        passwordInput.value,
+      );
+      localStorage.setItem('auth', loginResult.token);
+    } catch (error) {
+      alert('로그인 정보가 올바르지 않습니다.');
+    }
+  }
   return (
     <main className="main-base">
-      <form className="form-base">
+      <form className="form-base" onSubmit={handleSubmit}>
         <FormInput
           type="text"
           name="id_input"
@@ -25,7 +41,7 @@ export default function Login() {
             type="submit"
             name="login_button"
             value="로그인"
-            disabled
+            // disabled
             className="login-area-buttons mb-3 bg-blue-300 cursor-pointer opacity-50"
           />
           <Link to="/signup" className="login-area-buttons bg-red-300">

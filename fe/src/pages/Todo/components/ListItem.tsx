594f1a4825d4f50dc726131e996c7f218499ac02
godcl1623 <delphi5281@gmail.com>
2022-08-08 16:02:02 +0900

    #5 fix : 투두리스트에서 체크박스 로직 수정, 리스트에 수정/삭제 버튼 제거

--- fe/src/pages/Todo/components/ListItem.tsx
+++ fe/src/pages/Todo/components/ListItem.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { format } from 'date-fns';
 import { NavLink, useParams } from 'react-router-dom';
 import { TodoItemType } from 'types';
-import ModifyButton from 'components/ModifyButton';
 
 export default function ListItem({
   id,
@@ -11,39 +10,49 @@ export default function ListItem({
   createdAt,
   updatedAt,
 }: TodoItemType) {
+  const [isChecked, setIsChecked] = React.useState<boolean>(false);
   const params = useParams();
   const dateString =
     createdAt === updatedAt
       ? format(new Date(createdAt), 'yyyy-MM-dd')
       : format(new Date(updatedAt), 'yyyy-MM-dd');
+  const checkedString = isChecked ? 'line-through text-zinc-300' : '';
   return (
     <li className={`todo-list-item ${params.id === id ? 'active' : ''}`}>
-      <NavLink to={`/${id}/detail`} className="flex w-full h-full">
-        <label
-          htmlFor="checkbox"
-          className="inline-block w-[40px] h-[40px] border border-solid border-zinc-300 p-[2px] cursor-pointer"
-        >
-          <div className="w-full h-full bg-zinc-500" />
-          <input type="checkbox" name="checkbox" className="hidden" />
-        </label>
-        <section className="w-[60%] h-full">
-          <h1 className="h-1/2 py-2 px-3 text-3xl">{title}</h1>
-          <p className="h-1/2 py-2 px-3 text-zinc-400">{content}</p>
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+      <label
+        htmlFor="checkbox"
+        className="inline-block w-[40px] h-[40px] border border-solid border-zinc-300 p-[2px] cursor-pointer"
+        onClick={() => setIsChecked(!isChecked)}
+      >
+        <div
+          className={`w-full h-full ${
+            isChecked ? 'bg-zinc-500' : 'bg-transparent'
+          }`}
+        />
+        <input
+          type="checkbox"
+          name="checkbox"
+          className="hidden"
+          checked={isChecked}
+          onChange={() => undefined}
+        />
+      </label>
+      <NavLink to={`/${id}/detail`} className="flex items-center w-full h-full">
+        <section className="w-[65%] h-full">
+          <h1 className={`h-1/2 py-2 px-3 text-3xl ${checkedString}`}>
+            {title}
+          </h1>
+          <p className={`h-1/2 py-2 px-3 text-zinc-400 ${checkedString}`}>
+            {content}
+          </p>
         </section>
-        <section className="w-[30%] h-full">
-          <p className="flex-center h-1/2 pb-1 text-zinc-400">{dateString}</p>
-          <div className="flex-center justify-evenly h-1/2">
-            <ModifyButton
-              id={id}
-              title={title}
-              content={content}
-              createdAt={createdAt}
-              updatedAt={updatedAt}
-            />
-            <button type="button" className="button-alert">
-              삭제
-            </button>
-          </div>
+        <section className="flex items-end w-[35%] h-full">
+          <p
+            className={`flex-center w-full h-1/2 pb-1 text-zinc-400 ${checkedString}`}
+          >
+            {dateString}
+          </p>
         </section>
       </NavLink>
     </li>
